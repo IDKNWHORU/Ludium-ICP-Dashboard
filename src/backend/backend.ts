f495d3ad7fae97f8dbca45e61baaf363173b898d
IDKNWHORU <junho9341@gmail.com>
2024-05-16 14:04:05 +0900

    루디움 미션을 조회하는 기능 추가

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -3,8 +3,29 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/greet", (req, res) => {
-  res.json({ greeting: `Hello, ${req.query.name}` });
+app.post("/greet", async (req, res) => {
+  const missionSubmitListRes = await fetch(
+    "https://api.ludium.world/mission/34bb16b5-7e76-4988-9b49-802355a6fa22/submit"
+  );
+
+  const missionSubmitList = await missionSubmitListRes.json();
+  const resultPromises = missionSubmitList.map(
+    async ({ usrId, description, createAt, status }) => {
+      const userRes = await fetch(`https://api.ludium.world/user/${usrId}`);
+      const { nick } = await userRes.json();
+
+      return {
+        description,
+        createAt,
+        status,
+        nick,
+      };
+    }
+  );
+
+  const result = await Promise.all(resultPromises);
+
+  res.json(result);
 });
 
 app.use(express.static("/dist"));
--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,29 +1,46 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [submitList, setSubmitList] = useState([]);
+  const [pending, setPending] = useState(false);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${name}`)
-      .then(response => response.json()).then((json) => {
-        setGreeting(json.greeting)
-      });
+    setPending(true);
+    const submitListResponse = await fetch(
+      `${import.meta.env.VITE_CANISTER_URL}/greet`,
+      {
+        method: "POST",
+      }
+    );
+
+    setSubmitList(await submitListResponse.json());
+    setPending(false);
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+        <button type="submit">조회하기</button>
       </form>
-      <section id="greeting">{greeting}</section>
-    </main >
+      <section>
+        {pending ? "조회중..." : null}
+        {submitList.map((submit) => (
+          <article
+            key={submit.createAt + submit.nick}
+            style={{ border: "1px solid", padding: "15px", margin: "10px 0" }}
+          >
+            <h3>닉네임: {submit.nick}</h3>
+            <h4>
+              미션 제출 상태:{" "}
+              {submit.status === "APPROVE" ? "승인됨" : "제출됨"}
+            </h4>
+            <h5>제출 일시: {submit.createAt}</h5>
+            <code>제출 내용: {submit.description}</code>
+          </article>
+        ))}
+      </section>
+    </main>
   );
 }
 
